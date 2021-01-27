1191584067c42b3c9ae7c1b035369ac384f72aae
gaoyong0520 <gaoyong0520@outlook.com>
2021-01-27 19:45:27 +0800

    feat(list): 双向链表
    Signed-off-by: gaoyong0520 <gaoyong0520@outlook.com>

---
+++ src/common/datastructure/list/DoublyLinkedList.java
@@ -0,0 +1,15 @@
+package common.datastructure.list;
+
+/**
+ * @author 高勇01
+ * @date 2021/1/27 16:43
+ */
+public class DoublyLinkedList {
+    private Node node;
+
+    public void init() {
+        node = new Node(0, null, null);
+    }
+
+
+}
